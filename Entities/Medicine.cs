b84f2c5334b60ff880590d83449694a779644f7c
Juanes Cabrera <juanescabreraperez@gmail.com>
2026-04-27 13:44:30 -0500

    Delete Entities directory

--- Entities/Medicine.cs
+++
@@ -1,8 +0,0 @@
-namespace MiProyectoOnion.Domain.Entities;
-
-public class Medicine : BaseEntity
-{
-    public string Name { get; set; }
-    public string Dosis { get; set; }
-    public string Frecuency { get; set; }
-}
